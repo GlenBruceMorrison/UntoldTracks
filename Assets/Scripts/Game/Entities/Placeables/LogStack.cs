0f327f436c55521114432dc3f18f0c83eabbc5e7
G. Morrison <45969197+GlenBruceMorrison@users.noreply.github.com>
2022-05-22 21:24:52 +0100

    Added plank stack

---
+++ Assets/Scripts/Game/Entities/Placeables/LogStack.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UntoldTracks;
+using UntoldTracks.InventorySystem;
+using UntoldTracks.Player;
+
+public class LogStack : PlaceableEntity, IInteractable
+{
+    public Transform logContainer;
+    public List<Transform> logs = new List<Transform>();
+
+    public int currentStored = 0;
+    public int Max => logs.Count;
+    public Item logItem;
+    public string DisplayText => "RMB [Remove Log]\nLMB [Add Log]";
+    public Sprite DisplaySprite => source.sprite;
+    
+    private void Awake()
+    {
+        var index = 0;
+        foreach (Transform child in logContainer.transform)
+        {
+            logs.Add(child);
+
+            if (currentStored > index)
+            {
+                child.gameObject.SetActive(true);
+            }
+            else
+            {
+                child.gameObject.SetActive(false);
+            }
+
+            index++;
+        }
+    }
+
+    public bool AddLog()
+    {
+        if (currentStored >= Max)
+        {
+            return false;
+        }
+        
+        currentStored += 1;
+        logs[currentStored+1].gameObject.SetActive(true);
+        
+        return true;
+    }
+
+    public bool RemoveLog()
+    {
+        if (currentStored <= 0)
+        {
+            return false;
+        }
+        
+        currentStored -= 1;
+        logs[currentStored+2].gameObject.SetActive(false);
+        return true;
+    }
+    
+    public void HandlePrimaryInput(PlayerManager player, ItemContainer usingContainer)
+    {
+        if (!player.inventoryController.Inventory.CanTake(logItem, 1))
+        {
+            return;
+        }
+
+        if (!AddLog())
+        {
+            return;
+        }
+
+        player.inventoryController.Inventory.Take(new ItemQuery(logItem, 1));
+    }
+    
+    public void HandleSecondaryInput(PlayerManager player, ItemContainer usingContainer)
+    {
+        if (!player.inventoryController.Inventory.CanGive(logItem, 1))
+        {
+            return;
+        }
+
+        if (!RemoveLog())
+        {
+            return;
+        }
+        
+        player.inventoryController.Inventory.Give(new ItemContainer(logItem, 1));
+    }
+    
+    public void HandleBecomeFocus(PlayerManager player) { }
+    public void HandleLoseFocus(PlayerManager player) { }
+}
